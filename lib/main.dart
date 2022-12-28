81a57d5f9403df7c757c8e5ca89372c6a5c749c4
khadija-bibi <sub99way1122@gmail.com>
2022-12-28 09:56:37 +0500

    navigation and routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:task_7/home.dart';
-import 'package:task_7/show.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
   }
 }
-
